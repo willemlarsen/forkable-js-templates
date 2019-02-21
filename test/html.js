f9d19b56d5a7374daae4469266704ea5c856df72
Willem Larsen <willem@alchemycodelab.com>
2019-02-21 11:33:43 -0800

    html method not used anywhere

--- test/html.js
+++
@@ -1,9 +0,0 @@
-
-function html(strings, ...values) {
-    const rawHtml = String.raw(strings, ...values);
-    const template = document.createElement('template');
-    template.innerHTML = rawHtml;
-    return template.content; 
-}
-
-export default html;
