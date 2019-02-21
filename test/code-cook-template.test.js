994805e92db94f520333e2b0ecc9889e4e5c8e8c
Willem Larsen <willem@alchemycodelab.com>
2019-02-21 11:57:26 -0800

    remove code cooking tests so that apprentices can cook it

--- test/code-cook-template.test.js
+++ test/code-cook-template.test.js
@@ -2,36 +2,6 @@ const test = QUnit.test;
 
 QUnit.module('code cook template');
 
-test('make html from template with interpolation', function(assert) {
-    const place = 'world';
-    const html = `<p>hello ${place}</p>`;
-    assert.equal(html, '<p>hello world</p>');
-});
-
-
-test('call function that generates html template literal', function(assert) {
-    
-    function generateHtmlTemplate(place) {
-        return `<p>hello ${place}</p>`;
-    }
-
-    const result = generateHtmlTemplate('mars');
-    const expected = '<p>hello mars</p>';
-    assert.equal(result, expected);
-});
-
-test('what do template literals do?', function(assert) {
-    let resultText, interpolatedStuff;
-
-    function wat(strings, ...values) {
-        resultText = strings;
-        interpolatedStuff = values;
-    }
-
-    const num = 3;
-
-    wat`one${num}two`;
-
-    assert.deepEqual(resultText, ['one', 'two']);
-    assert.deepEqual(interpolatedStuff, [3]);
+test('hello', function(assert) {
+    assert.equal(true, true);
 });
